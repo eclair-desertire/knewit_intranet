5b5f08f89b514bf0e4e2a6524deb6041e2c44c07
Alisher Ruziyev <eclair.desertire@gmail.com>
2021-07-17 10:54:51 +0600

    added first scratch of course field

--- intranet/models.py
+++ intranet/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class Course(models.Model):
+    course_name=models.CharField(max_length=200)
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
 
 class Video(models.Model):
     name= models.CharField(max_length=500)
